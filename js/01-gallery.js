be1ce8069386221c979e25aa10a9e392a2d940f1
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-11 16:10:47 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,9 +22,9 @@ function onClickList(evt) {
     if (evt.code !== 'Escape') {
       return;
     }
-    // console.log(evt);
+    console.log(evt);
     instance.close();
-    // window.removeEventListener('keydown', onEscKayPress);
+    window.removeEventListener('keydown', onEscKayPress);
   }
 }
 
@@ -54,57 +54,3 @@ linkElementGallery.addEventListener('click', onClickLinkElementGalleryPreventDef
 function onClickLinkElementGalleryPreventDefault(evt) {
   evt.preventDefault();
 }
-
-// const list = document.querySelector('.gallery');
-
-// console.log(createGalleryMarkup(galleryItems));
-
-// list.addEventListener('click', onClickList);
-
-// function createGalleryMarkup(galleryItems) {
-//   const elementGalleryMarkup = galleryItems
-//     .map(({ description, original, preview }) => {
-//       return `
-//     <li class="gallery__item">
-//     <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </li>`;
-//     })
-//     .join('');
-
-//   list.insertAdjacentHTML('beforeend', elementGalleryMarkup);
-// }
-
-// function onClickList(evt) {
-//   if (!evt.target.classList.contains('gallery__image')) {
-//     return;
-//   }
-//   const instance = basicLightbox.create(
-//     `<img class="gallery__image-click-on" src="${evt.target.dataset.source}" width="800" height="600">`
-//   );
-//   instance.show();
-
-//   onClickLinkElementGalleryPreventDefault(evt);
-
-//   window.addEventListener('keydown', onEscKayPress);
-
-//   function onEscKayPress(evt) {
-//     if (evt.code !== 'Escape') {
-//       return;
-//     }
-//     instance.close();
-//     window.removeEventListener('keydown', onEscKayPress);
-//   }
-// }
-
-// const linkElementGallery = document.querySelector('.gallery__link');
-// linkElementGallery.addEventListener('click', onClickLinkElementGalleryPreventDefault);
-// function onClickLinkElementGalleryPreventDefault(evt) {
-//   evt.preventDefault();
-// }
