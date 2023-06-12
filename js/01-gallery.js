685deed0321a4682992054684e4498b1ad8f70e9
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-12 08:49:45 +0300

    Change task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,37 @@
 import { galleryItems } from './gallery-items.js';
+
 const list = document.querySelector('.gallery');
 list.addEventListener('click', onClickList);
 
 console.log(createGalleryMarkup(galleryItems));
 
+const instance = basicLightbox.create(
+  `<img class="gallery__image-modal" src="" width="800" height="600">`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', onEscKayPress);
+    },
+    onClose: instance => {
+      window.removeEventListener('keydown', onEscKayPress);
+    },
+  }
+);
+
 function onClickList(evt) {
+  evt.preventDefault();
   if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
+  instance.element().querySelector('img').src = evt.target.dataset.source;
 
-  const instance = basicLightbox.create(
-    `<img class="gallery__image-click-on" src="${evt.target.dataset.source}" width="800" height="600">`
-  );
   instance.show();
+}
 
-  onClickLinkElementGalleryPreventDefault(evt);
-
-  window.addEventListener('keydown', onEscKayPress);
-  // window.addEventListener('keydown', onEscKayPress, { once: true });
-
-  function onEscKayPress(evt) {
-    if (evt.code !== 'Escape') {
-      return;
-    }
-    instance.close();
-    window.removeEventListener('keydown', onEscKayPress);
+function onEscKayPress(evt) {
+  if (evt.code !== 'Escape') {
+    return;
   }
+  instance.close();
 }
 
 function createGalleryMarkup(galleryItems) {
@@ -40,17 +46,68 @@ function createGalleryMarkup(galleryItems) {
       data-source="${original}"
       alt="${description}"
     />
-  </a>
-</li>`;
+    </a>
+    </li>`;
     })
     .join('');
 
   list.insertAdjacentHTML('beforeend', elementGalleryMarkup);
 }
 
-const linkElementGallery = document.querySelector('.gallery__link');
-linkElementGallery.addEventListener('click', onClickLinkElementGalleryPreventDefault);
+// як не потрібно робити
 
-function onClickLinkElementGalleryPreventDefault(evt) {
-  evt.preventDefault();
-}
+// const list = document.querySelector('.gallery');
+// list.addEventListener('click', onClickList);
+
+// console.log(createGalleryMarkup(galleryItems));
+
+// function onClickList(evt) {
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+
+//   const instance = basicLightbox.create(
+//     `<img class="gallery__image-click-on" src="${evt.target.dataset.source}" width="800" height="600">`
+//   );
+//   instance.show();
+
+//   onClickLinkElementGalleryPreventDefault(evt);
+
+//   window.addEventListener('keydown', onEscKayPress);
+//   // window.addEventListener('keydown', onEscKayPress, { once: true });
+
+//   function onEscKayPress(evt) {
+//     if (evt.code !== 'Escape') {
+//       return;
+//     }
+//     instance.close();
+//     window.removeEventListener('keydown', onEscKayPress);
+//   }
+// }
+
+// function createGalleryMarkup(galleryItems) {
+//   const elementGalleryMarkup = galleryItems
+//     .map(({ description, original, preview }) => {
+//       return `
+//     <li class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`;
+//     })
+//     .join('');
+
+//   list.insertAdjacentHTML('beforeend', elementGalleryMarkup);
+// }
+
+// const linkElementGallery = document.querySelector('.gallery__link');
+// linkElementGallery.addEventListener('click', onClickLinkElementGalleryPreventDefault);
+
+// function onClickLinkElementGalleryPreventDefault(evt) {
+//   evt.preventDefault();
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,23 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const list = document.querySelector('.gallery');
-list.addEventListener('click', onClickList);
-
-console.log(createGalleryMarkup(galleryItems));
-
-console.log(list);
+createGalleryMarkup();
 var lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
 
-function onClickList(evt) {
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-}
-
-function createGalleryMarkup(galleryItems) {
+function createGalleryMarkup() {
   const elementGalleryMarkup = galleryItems
     .map(({ description, original, preview }) => {
       return `
@@ -29,10 +19,47 @@ function createGalleryMarkup(galleryItems) {
       src="${preview}"
       alt="${description}"
     />
-  </a>
-</li>`;
+    </a>
+    </li>`;
     })
     .join('');
 
   list.insertAdjacentHTML('beforeend', elementGalleryMarkup);
 }
+
+// як не потрібно робити
+// const list = document.querySelector('.gallery');
+// list.addEventListener('click', onClickList);
+
+// console.log(createGalleryMarkup(galleryItems));
+
+// console.log(list);
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+//   captionsData: 'alt',
+// });
+
+// function onClickList(evt) {
+//   if (!evt.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+// }
+
+// function createGalleryMarkup(galleryItems) {
+//   const elementGalleryMarkup = galleryItems
+//     .map(({ description, original, preview }) => {
+//       return `
+//     <li class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`;
+//     })
+//     .join('');
+
+//   list.insertAdjacentHTML('beforeend', elementGalleryMarkup);
+// }
